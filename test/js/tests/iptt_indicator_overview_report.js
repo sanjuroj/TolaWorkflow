de6e1f226f209a863e95e1d8f0a0af2f8013259b
Kurt Wall <kwall@kurtwerks.com>
2018-04-25 17:23:35 -0700

    Move report-specific tests to their own file

---
+++ test/js/tests/iptt_indicator_overview_report.js
@@ -0,0 +1,36 @@
+import IpttPage from '../pages/iptt.page';
+import LoginPage from '../pages/login.page';
+import Util from '../lib/testutil';
+import { assert, expect } from 'chai';
+'use strict';
+
+/**
+ * IPTT report: Program indicator overview report
+ * Tests from mc/issues/119
+ */
+describe('Program indicator overview report', function() {
+    before(function() {
+        // Disable timeouts
+        this.timeout(0);
+        browser.windowHandleMaximize();
+        let parms = Util.readConfig();
+
+        LoginPage.open(parms.baseurl);
+        if (parms.baseurl.includes('mercycorps.org')) {
+            LoginPage.username = parms.username;
+            LoginPage.password = parms.password;
+            LoginPage.login.click();
+        } else if (parms.baseurl.includes('localhost')) {
+            LoginPage.googleplus.click();
+            if (LoginPage.title != 'TolaActivity') {
+                LoginPage.gUsername = parms.username + '@mercycorps.org';
+                LoginPage.gPassword = parms.password;
+            }
+        }        
+    });
+
+    it('should display Targets and % Met fields for LoP target and actual');
+    it('should require the start month to be older than the end date');
+    it('should only display LoP targets, not intermediate targets');
+    it('should only display % Met for LoP targets, not intermediate targets');
+}); 
