7065f417b24027c72be14e19ea9c09ee8dc5533b
Kurt Wall <kwall@kurtwerks.com>
2018-05-17 03:00:42 -0700

    Fix another couple of tests
    Also, fix indentation

--- test/js/tests/quarterly.js
+++ test/js/tests/quarterly.js
@@ -28,68 +28,68 @@ describe('Quarterly target frequency', function() {
   })
 
   it('should require date that first target period begins', function() {
-  NavBar.Indicators.click()
-  expect('Program Indicators' === IndPage.getPageName())
-  IndPage.createBasicIndicator()
-  TargetsTab.setIndicatorName('Quarterly target first period required testing')
-  TargetsTab.setUnitOfMeasure('Hawks per hectare')
-  TargetsTab.setLoPTarget(31)
-  TargetsTab.setBaseline(32)
-  TargetsTab.setTargetFrequency('Quarterly')
-
-  // Trying to save without setting the start date should fail
-  TargetsTab.saveIndicatorChanges()
-  let errorMessage = TargetsTab.getTargetFirstPeriodErrorHint()
-  expect(true === errorMessage.includes('Please complete this field.'))
+    NavBar.Indicators.click()
+    expect('Program Indicators' === IndPage.getPageName())
+    IndPage.createBasicIndicator()
+    TargetsTab.setIndicatorName('Quarterly target first period required testing')
+    TargetsTab.setUnitOfMeasure('Hawks per hectare')
+    TargetsTab.setLoPTarget(31)
+    TargetsTab.setBaseline(32)
+    TargetsTab.setTargetFrequency('Quarterly')
+  
+    // Trying to save without setting the start date should fail
+    TargetsTab.saveIndicatorChanges()
+    let errorMessage = TargetsTab.getTargetFirstPeriodErrorHint()
+    expect(true === errorMessage.includes('Please complete this field.'))
   })
 
   it('should default number of periods to 1', function() {
-  expect(1 === TargetsTab.getNumTargetPeriods())
+    expect(1 === TargetsTab.getNumTargetPeriods())
   })
 
   it('should create target periods for each period requested', function() {
-  IndPage.clickIndicatorsLink()
-  IndPage.createBasicIndicator()
-
-  // This should succeed
-  TargetsTab.setIndicatorName('Quarterly target create target periods testing')
-  TargetsTab.setUnitOfMeasure('Irritants per island')
-  TargetsTab.setLoPTarget(49)
-  TargetsTab.setBaseline(50)
-  TargetsTab.setTargetFrequency('Quarterly')
-
-  TargetsTab.setNumTargetPeriods(4)
-  TargetsTab.saveIndicatorChanges()
-  expect(4 === TargetsTab.getNumTargetPeriods())
+    IndPage.clickIndicatorsLink()
+    IndPage.createBasicIndicator()
+  
+    // This should succeed
+    TargetsTab.setIndicatorName('Quarterly target create target periods testing')
+    TargetsTab.setUnitOfMeasure('Irritants per island')
+    TargetsTab.setLoPTarget(49)
+    TargetsTab.setBaseline(50)
+    TargetsTab.setTargetFrequency('Quarterly')
+    TargetsTab.setFirstTargetPeriod()
+    TargetsTab.setNumTargetPeriods(4)
+    TargetsTab.saveIndicatorChanges()
+    expect(4 === TargetsTab.getNumTargetPeriods())
   })
 
   it('should require entering targets for each target period', function() {
-  IndPage.clickIndicatorsLink()
-  IndPage.createBasicIndicator()
-
-  TargetsTab.setIndicatorName('Quarterly target, target period value(s) required')
-  TargetsTab.setUnitOfMeasure('Weekends per worker')
-  TargetsTab.setLoPTarget(65)
-  TargetsTab.setBaseline(66)
-  TargetsTab.setTargetFrequency('Quarterly')
-  TargetsTab.setNumTargetPeriods(4)
-  TargetsTab.setFirstTargetPeriod()
-  TargetsTab.saveIndicatorChanges()
-
-  // Find the input boxes
-  let inputBoxes = TargetsTab.getTargetInputBoxes()
-  let targetCount = inputBoxes.length
-  // Place values in each box one at a time and attempt to save.
-  // This should *fail* until all the fields are filled.
-  let errorCount = 0
-  for(let inputBox of inputBoxes) {
-    inputBox.setValue(86)
+    IndPage.clickIndicatorsLink()
+    IndPage.createBasicIndicator()
+  
+    TargetsTab.setIndicatorName('Quarterly target, target period value(s) required')
+    TargetsTab.setUnitOfMeasure('Weekends per worker')
+    TargetsTab.setLoPTarget(65)
+    TargetsTab.setBaseline(66)
+    TargetsTab.setTargetFrequency('Quarterly')
+    TargetsTab.setNumTargetPeriods(4)
+    TargetsTab.setFirstTargetPeriod()
     TargetsTab.saveIndicatorChanges()
-    // Did we fail successfully?
-    let errMsg = TargetsTab.getTargetValueErrorHint()
-    expect(true === errMsg.includes('Please enter a target value.'))
-    errorCount++
-  }
-  expect(targetCount === errorCount)
+  
+    // Find the input boxes
+    let inputBoxes = TargetsTab.getTargetInputBoxes()
+    let targetCount = inputBoxes.length
+    // Place values in each box one at a time and attempt to save.
+    // This should *fail* until all the fields are filled.
+    let errorCount = targetCount
+    for(let inputBox of inputBoxes) {
+      inputBox.setValue(86)
+      TargetsTab.saveIndicatorChanges()
+      // Did we fail successfully? If not, she's gonna blow Captain!
+      let errMsg = TargetsTab.getTargetValueErrorHint()
+      expect(true === errMsg.includes('Please enter a target value.'))
+      errorCount--
+    }
+    expect(0 === errorCount)
   })
 })
--- test/js/tests/triannual.js
+++ test/js/tests/triannual.js
@@ -28,69 +28,71 @@ describe('Tri-annual target frequency', function() {
   })
 
   it('should require date that first target period begins', function() {
-  NavBar.Indicators.click()
-  expect('Program Indicators' === IndPage.getPageName())
-  IndPage.createBasicIndicator()
+    NavBar.Indicators.click()
+    expect('Program Indicators' === IndPage.getPageName())
+    IndPage.createBasicIndicator()
 
-  // This should succeed
-  TargetsTab.setIndicatorName('Tri-annual target first period required testing')
-  TargetsTab.setUnitOfMeasure('Jugs per jalopy')
-  TargetsTab.setLoPTarget(29)
-  TargetsTab.setBaseline(30)
-  TargetsTab.setTargetFrequency('Tri-annual')
+    // This should succeed
+    TargetsTab.setIndicatorName('Tri-annual target first period required testing')
+    TargetsTab.setUnitOfMeasure('Jugs per jalopy')
+    TargetsTab.setLoPTarget(29)
+    TargetsTab.setBaseline(30)
+    TargetsTab.setTargetFrequency('Tri-annual')
 
-  // Trying to save without setting the start date should fail
-  TargetsTab.saveIndicatorChanges()
-  let errorMessage = TargetsTab.getTargetFirstPeriodErrorHint()
-  expect(true === errorMessage.includes('Please complete this field.'))
+    // Trying to save without setting the start date should fail
+    TargetsTab.saveIndicatorChanges()
+    let errorMessage = TargetsTab.getTargetFirstPeriodErrorHint()
+    expect(true === errorMessage.includes('Please complete this field.'))
   })
 
   it('should default number of periods to 1', function() {
-  expect(1 === TargetsTab.getNumTargetPeriods())
+    expect(1 === TargetsTab.getNumTargetPeriods())
   })
 
   it('should create target periods for each period requested', function() {
-  IndPage.clickIndicatorsLink()
-  IndPage.createBasicIndicator()
+    IndPage.clickIndicatorsLink()
+    IndPage.createBasicIndicator()
 
-  // This should succeed
-  TargetsTab.setIndicatorName('Tri-annual target first period required testing')
-  TargetsTab.setUnitOfMeasure('Krogers per Kurt')
-  TargetsTab.setLoPTarget(49)
-  TargetsTab.setBaseline(50)
-  TargetsTab.setTargetFrequency('Tri-annual')
-  TargetsTab.setNumTargetPeriods(3)
-  TargetsTab.saveIndicatorChanges()
-  expect(3 === TargetsTab.getNumTargetPeriods())
+    // This should succeed
+    TargetsTab.setIndicatorName('Tri-annual target, create requested targets')
+    TargetsTab.setUnitOfMeasure('Krogers per Kurt')
+    TargetsTab.setLoPTarget(49)
+    TargetsTab.setBaseline(50)
+    TargetsTab.setTargetFrequency('Tri-annual')
+    TargetsTab.setFirstTargetPeriod()
+    TargetsTab.setNumTargetPeriods(4)
+    TargetsTab.saveIndicatorChanges()
+    expect(4 === TargetsTab.getNumTargetPeriods())
   })
 
   it('should require entering targets for each target period', function() {
-  IndPage.clickIndicatorsLink()
-  IndPage.createBasicIndicator()
+    IndPage.clickIndicatorsLink()
+    IndPage.createBasicIndicator()
 
-  TargetsTab.setIndicatorName('Tri-annual target, target period value(s) required')
-  TargetsTab.setUnitOfMeasure('Trouble per tribble')
-  TargetsTab.setLoPTarget(308)
-  TargetsTab.setBaseline(309)
-  TargetsTab.setTargetFrequency('Tri-annual')
-  TargetsTab.setNumTargetPeriods(3)
-  TargetsTab.setFirstTargetPeriod()
-  TargetsTab.saveIndicatorChanges()
-
-  // Find the input boxes
-  let inputBoxes = TargetsTab.getTargetInputBoxes()
-  let targetCount = inputBoxes.length
-  // Place values in each box one at a time and attempt to save.
-  // This should *fail* until all the fields are filled.
-  let errorCount = 0
-  for(let inputBox of inputBoxes) {
-    inputBox.setValue(82)
+    TargetsTab.setIndicatorName('Tri-annual target, target period value(s) required')
+    TargetsTab.clickTargetsTab()
+    TargetsTab.setUnitOfMeasure('Trouble per tribble')
+    TargetsTab.setLoPTarget(308)
+    TargetsTab.setBaseline(309)
+    TargetsTab.setTargetFrequency('Tri-annual')
+    TargetsTab.setNumTargetPeriods(4)
+    TargetsTab.setFirstTargetPeriod()
     TargetsTab.saveIndicatorChanges()
-    // Did we fail successfully?
-    let errMsg = TargetsTab.getTargetValueErrorHint()
-    expect(true === errMsg.includes('Please enter a target value.'))
-    errorCount++
-  }
-  expect(targetCount === errorCount)
+
+    // Find the input boxes
+    let inputBoxes = TargetsTab.getTargetInputBoxes()
+    let targetCount = inputBoxes.length
+    // Place values in each box one at a time and attempt to save.
+    // This should *fail* until all the fields are filled.
+    let errorCount = targetCount
+    for(let inputBox of inputBoxes) {
+      inputBox.setValue(82)
+      TargetsTab.saveIndicatorChanges()
+      // Did we fail successfully?
+      let errMsg = TargetsTab.getTargetValueErrorHint()
+      expect(true === errMsg.includes('Please enter a target value.'))
+      errorCount--
+    }
+    expect(0 === errorCount)
   })
 })
