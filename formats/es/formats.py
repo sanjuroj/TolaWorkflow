424bd9a62f23687a24ff94dc919fcfc8c3f8c476
Paul Souders <paul@axoplasm.com>
2018-08-10 14:15:55 -0700

    Explicit i/o date format
    goal is to ensure we specify the purpose of the format. DATE_FORMAT is too ambiguous. Use IO_DATE_FORMAT for inputs and hidden fields. Use MEDIUM_DATE_FORMAT in most other places

--- formats/es/formats.py
+++ formats/es/formats.py
@@ -1,5 +1,6 @@
-DATE_FORMAT = 'Y-m-d' # 2018-07-08 # DEFAULT
+DATE_FORMAT = 'Y-m-d' # 2018-07-08 # fallback, Django default
+IO_DATE_FORMAT = 'Y-m-d' # 2018-07-08 # Default for i/o
 LONG_DATE_FORMAT = 'j F Y' # 15 Abril 2018
-MEDIUM_DATE_FORMAT = 'j N Y' # 15 Abr. 2018
+MEDIUM_DATE_FORMAT = 'j N Y' # 15 Abr. 2018 # Default for display
 SHORT_DATE_FORMAT = 'Y-m-d' # 2018-04-08
 ISO_DATETIME = 'Y-m-d\TH:i:sO' # 2018-07-08T17:02:56-08:00
