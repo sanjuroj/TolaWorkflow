59acb47c86a46d09a005c331bb3b2170095ce857
Paul Souders <paul@axoplasm.com>
2018-10-15 12:46:07 -0700

    Incomplete implementation of #564 as a non-nested list in a templatetag

---
+++ workflow/templatetags/menu_tag.py
@@ -0,0 +1,14 @@
+from django import template
+from workflow.models import Program
+
+register = template.Library()
+
+@register.inclusion_tag('workflow/tags/program_menu.html', takes_context=True)
+def program_menu(context):
+    request = context['request']
+    #programs = Program.objects.all()
+    countries = request.user.tola_user.countries.all()
+    programs = Program.objects.filter(funding_status="Funded", country__in=countries).distinct()
+    return {
+        'programs': programs,
+    }
