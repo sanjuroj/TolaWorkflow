9b5dac0a5ae90da85beb84ec4ab07265b7011e06
Kurt Wall <kwall@kurtwerks.com>
2018-10-24 05:38:57 -0700

    Remove suite definitions

--- test/js/wdio.gecko.conf.js
+++ test/js/wdio.gecko.conf.js
@@ -6,53 +6,13 @@ exports.config = {
   exclude: [
     // 'path/to/excluded/files'
   ],
-  suites: {
-    dashboard: [
-      'tests/dashboard.js'
-    ],
-    evidence: [
-      'tests/evidence_table_num_ind.js',
-      'tests/evidence_table_pct_ind.js',
-      'tests/indicator_evidence_dropdown.js'
-    ],
-    indicators: [
-      'tests/indicators_landing_page.js',
-      'tests/indicators_table.js',
-      'tests/create_indicator_form.js',
-      'tests/indicator_detail_form.js',
-      'tests/num_ind_config_display.js',
-      'tests/num_pct_ind.js'
-    ],
-    iptt: [
-      'tests/iptt_landing_page.js',
-      'tests/iptt_indicator_overview.js',
-      'tests/iptt_target_overview.js',
-      'tests/iptt_indicator_overview_report.js',
-      'tests/iptt_target_overview_report.js',
-      'tests/report_filter_panel.js',
-      'tests/report_sort_columns.js'
-    ],
-    login: [
-      'tests/00_login.js'
-    ],
-    periodic: [
-      'tests/event.js',
-      'tests/lop_only.js',
-      'tests/mid-end_line.js',
-      'tests/semiannual.js',
-      'tests/triannual.js',
-      'tests/monthly.js',
-      'tests/quarterly.js',
-      'tests/periodic_date_ranges.js'
-    ]
-  },
   // Capabilities
-  maxInstances: 1,
+  maxInstances: 2,
   capabilities: [
     {
       browserName: 'firefox',
       maxInstances: 1,
-      args: '[--jsdebugger]',
+      //args: '[--jsdebugger]',
       'moz:firefoxOptions': {
       // []
       }
