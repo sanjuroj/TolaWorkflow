78388fe9ff2fe41ca7dd3eee635a81a63a9cf6ee
Ken Johnson <kjohnson@mercycorps.org>
2018-10-25 16:00:40 -0700

    A select2 based program nav menu

--- workflow/templatetags/menu_tag.py
+++ workflow/templatetags/menu_tag.py
@@ -1,14 +1,27 @@
+from collections import OrderedDict
+
 from django import template
 from workflow.models import Program
 
 register = template.Library()
+
 
 @register.inclusion_tag('workflow/tags/program_menu.html', takes_context=True)
 def program_menu(context):
     request = context['request']
     countries = request.user.tola_user.countries.all()
     programs = Program.objects.filter(funding_status="Funded", country__in=countries).distinct()
+
+    programs_by_country = OrderedDict((country.country, []) for country in countries)
+
+    for program in programs:
+        for country in program.country.all():
+            # a program can be in multiple countries, including a country a user is not privy to
+            if country.country in programs_by_country:
+                programs_by_country[country.country].append(program)
+
     return {
         'programs': programs,
         'countries': countries,
+        'programs_by_country': programs_by_country,
     }
