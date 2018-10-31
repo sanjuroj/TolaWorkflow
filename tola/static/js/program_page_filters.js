c0104f4b7b6278c618a86e834f554ab06e1c1327
Paul Souders <paul@axoplasm.com>
2018-10-30 18:04:47 -0700

    filtering JS belongs in its own file

---
+++ tola/static/js/program_page_filters.js
@@ -0,0 +1,35 @@
+function hide_row_factory(positive, target) {
+    return function() {
+        var this_show;
+        if (target == "results_evidence") {
+            this_show = $(this).data('has-evidence') * positive;
+        } else if (target == "reported_results") {
+            this_show = $(this).data('reported-results') * positive;
+        } else if (target == "targets_defined") {
+            this_show = $(this).data('defined-targets') * positive;
+        }
+        if (this_show == -1) {
+            $(this).hide();
+            $(this).next().hide();
+        } else  {
+            $(this).show();
+            $(this).next().show();
+        }
+    }
+}
+$(document).ready(function() {
+    $('.filter-trigger').on('click', function(e) {
+        e.preventDefault();
+        var target, positive;
+        var highlighted_tab = $(this).parents('.gauge');
+        target = $(this).data('target');
+        positive = $(this).data('target-positive');
+        if (positive === 0) {
+            return;
+        }
+        callback = hide_row_factory(positive, target);
+        $('.indicators-list__indicator-header').each(callback);
+        $('.gauge').removeClass('is-highlighted');
+        highlighted_tab.addClass('is-highlighted');
+    });
+});
