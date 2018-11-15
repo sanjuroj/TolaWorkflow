54f55427613e337c47dbd51cc10e7db2e1986275
Paul Souders <paul@axoplasm.com>
2018-11-15 13:07:29 -0800

    filter highlighting (this & 2 prev commits address #561)

--- tola/static/js/program_page_filters.js
+++ tola/static/js/program_page_filters.js
@@ -91,7 +91,7 @@ $(document).ready(function() {
     });
 
     // gauge band links (indicators on track gas tank)
-    $('span[data-over-under-filter]').on('click', function (e) {
+    $('.filter-trigger--band').on('click', function (e) {
         e.preventDefault();
         let elem = $(this);
 
