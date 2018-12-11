03d7b950d8c11ac02751c245b098e9d5cdd4e1e9
Ken Johnson <kjohnson@mercycorps.org>
2018-12-11 12:17:24 -0800

    Fix tests

--- indicators/tests/test_indicator_import.py
+++ indicators/tests/test_indicator_import.py
@@ -10,10 +10,10 @@ class ImportIndicatorTests(TestCase):
     @mock.patch("indicators.views.views_indicators.requests.get")
     def test_remote_resource_true(self, mock_get):
         """
-        It should deserialize the response content if deserialize is left
-        true by default
+        It should deserialize the response content
         """
         class mock_response(object):
+            json = lambda self: {'hello': 1}
             content = '{"hello": 1}'
 
         mock_get.return_value = mock_response()
@@ -21,18 +21,3 @@ class ImportIndicatorTests(TestCase):
         result = import_indicator(service=service.id)
 
         self.assertDictEqual(result, {'hello': 1})
-
-    @mock.patch("indicators.views.views_indicators.requests.get")
-    def test_remote_resource_false(self, mock_get):
-        """
-        It should return the response object if deserialize is set to anything
-        other than bool True
-        """
-
-        class mock_response(object):
-            content = '{"hello": 1}'
-
-        mock_get.return_value = mock_response()
-        service = ExternalServiceFactory(name="Import Ind Test Service")
-        result = import_indicator(service=service.id, deserialize='')
-        self.assertIsInstance(result, mock_response)
