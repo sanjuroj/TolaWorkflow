3887c51d368c6fd3dcbb5c188e5764aaf79aa0ba
Joseph Spitz <jspitz@parthenonsoftware.com>
2019-02-19 12:32:19 -0800

    override list method of viewset not necessary for countries

--- tola_management/countryadmin.py
+++ tola_management/countryadmin.py
@@ -69,7 +69,6 @@ class CountryAdminSerializer(serializers.ModelSerializer):
         return ret
 
 
-
 class CountryAdminViewSet(viewsets.ModelViewSet):
     serializer_class = CountryAdminSerializer
     pagination_class = Paginator
@@ -100,12 +99,3 @@ class CountryAdminViewSet(viewsets.ModelViewSet):
             )
 
         return queryset.distinct()
-
-    def list(self, request):
-        queryset = self.get_queryset()
-        page = self.paginate_queryset(list(queryset))
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
