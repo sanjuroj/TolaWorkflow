b8a0ff198db4bd85dee7ee4a06623c3b81363ea1
Sanjuro Jogdeo <sjogdeo@mercycorps.org>
2019-02-21 14:56:47 -0800

    build bundles

--- build/dev-dist/base-c0639eb5b60e157115c0.js
+++ build/dev-dist/base-c0639eb5b60e157115c0.js
@@ -309,4 +309,4 @@ window.newPopup = newPopup; // EXAMPLE: <a onclick="newPopup('https://docs.googl
 /***/ })
 
 },[["YqHn","runtime"]]]);
-//# sourceMappingURL=base-8a08ae4372b017a8eb89.js.map
+//# sourceMappingURL=base-c0639eb5b60e157115c0.js.map
