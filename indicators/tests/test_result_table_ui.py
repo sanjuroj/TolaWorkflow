e34904018cc37a6a5b5c3b2c41ac6f3f6a9a91e2
Ken Johnson <kjohnson@mercycorps.org>
2019-02-26 13:06:02 -0800

    Functional tests for results table
    Covers first 2 tickets on list.
    
    Closes #1085
    Closes #1084

---
+++ indicators/tests/test_result_table_ui.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Test result table view for common regressions
+"""
+import datetime
+
+from django import test
+from django.urls import reverse_lazy
+
+from factories import ProgramFactory, IndicatorFactory, ResultFactory, PeriodicTargetFactory
+from indicators.models import Indicator
+
+
+class TestResultUnassignedUIDateFallsOutsideProgramPeriod(test.TestCase):
+    """
+    A result is unassigned because its "date collected" falls outside of the program period AND targets are time-aware
+
+    When a result is unassigned because its "date collected" falls outside of the program period AND
+    targets are time-aware, the following are true:
+
+    1. The result is displayed in the table underneath the target period rows.
+    2. Under the table, we display error message: This date falls outside the range of your target periods.
+       Please select a date between [localized program start date] and [localized program end date].
+    """
+
+    def setUp(self):
+        self.program = ProgramFactory(
+            reporting_period_start=datetime.date(2018, 1, 1),
+            reporting_period_end=datetime.date(2019, 1, 1),
+        )
+        self.indicator = IndicatorFactory(
+            program=self.program,
+            target_frequency=Indicator.ANNUAL,
+        )
+        PeriodicTargetFactory(
+            indicator=self.indicator,
+            start_date=datetime.date(2018, 1, 1),
+            end_date=datetime.date(2018, 12, 31),
+        )
+        self.result = ResultFactory(
+            indicator=self.indicator,
+            date_collected=datetime.date(2017, 1, 1),
+            achieved=42,
+            record_name='My Test Record',
+            evidence_url='http://my_evidence_url',
+        )
+
+    def test_result_table_html(self):
+        url = reverse_lazy('result_view', args=[self.indicator.id, self.program.id])
+        response = self.client.get(url)
+        # result is displayed
+        self.assertContains(
+            response,
+            'Jan 1, 2017',
+        )
+        self.assertContains(response, '42', count=3)
+        # expected warning message
+        self.assertContains(
+            response,
+            'This date falls outside the range of your target periods. Please select a date between Jan 1, 2018 and Jan 1, 2019.'
+        )
+
+
+class TestResultUnassignedUITargetsMidlineEndline(test.TestCase):
+    """
+    A result is unassigned because its "date collected" falls outside of the program period AND
+    targets are midline/endline or event
+
+    When a result is unassigned AND targets are Midline/Endline or Event, the following are true:
+
+    1. The result is displayed in the table.
+    2. Under the table, we display error message: This record is not associated with a target.
+       Open the data record and select an option from the "Measure against target" menu.
+    """
+
+    def setUp(self):
+        self.program = ProgramFactory(
+            reporting_period_start=datetime.date(2018, 1, 1),
+            reporting_period_end=datetime.date(2019, 1, 1),
+        )
+        self.indicator = IndicatorFactory(
+            program=self.program,
+            target_frequency=Indicator.MID_END,
+        )
+        PeriodicTargetFactory(
+            indicator=self.indicator,
+            start_date=datetime.date(2018, 1, 1),
+            end_date=datetime.date(2018, 12, 31),
+        )
+        self.result = ResultFactory(
+            indicator=self.indicator,
+            date_collected=datetime.date(2017, 1, 1),
+            achieved=42,
+            record_name='My Test Record',
+            evidence_url='http://my_evidence_url',
+        )
+
+    def test_result_table_html(self):
+        url = reverse_lazy('result_view', args=[self.indicator.id, self.program.id])
+        response = self.client.get(url)
+        # print response.content
+        # result is displayed
+        self.assertContains(
+            response,
+            'Jan 1, 2017',
+        )
+        self.assertContains(response, '42', count=3)
+        # expected warning message
+        self.assertContains(
+            response,
+            'This record is not associated with a target. Open the data record and select an option from the “Measure against target” menu.'
+        )
