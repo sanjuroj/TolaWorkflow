d674c172a2db707f822b80f9f955927f24fb846b
Ken Johnson <risinglight@gmail.com>
2019-02-27 13:56:28 -0800

    Test UI when result is unassigned and no targets are set up
    Closes #1086

--- indicators/tests/test_result_table_ui.py
+++ indicators/tests/test_result_table_ui.py
@@ -98,7 +98,6 @@ class TestResultUnassignedUITargetsMidlineEndline(test.TestCase):
     def test_result_table_html(self):
         url = reverse_lazy('result_view', args=[self.indicator.id, self.program.id])
         response = self.client.get(url)
-        # print response.content
         # result is displayed
         self.assertContains(
             response,
@@ -109,4 +108,50 @@ class TestResultUnassignedUITargetsMidlineEndline(test.TestCase):
         self.assertContains(
             response,
             'This record is not associated with a target. Open the data record and select an option from the “Measure against target” menu.'
+        )
+
+
+class TestResultUnassignedUITargetsNotSetup(test.TestCase):
+    """
+    When a result is unassigned because no targets are set up, the following are true:
+
+    1. The result is displayed in the table.
+    2. Under the table, we display error message: Targets are not set up for this indicator. Start by selecting a target frequency.
+    3. The string "Start by selecting a target frequency" links to the Targets tab of the indicator setup form.
+    """
+
+    def setUp(self):
+        self.program = ProgramFactory(
+            reporting_period_start=datetime.date(2018, 1, 1),
+            reporting_period_end=datetime.date(2019, 1, 1),
+        )
+        self.indicator = IndicatorFactory(
+            program=self.program,
+            target_frequency=Indicator.MID_END,
+        )
+        self.result = ResultFactory(
+            indicator=self.indicator,
+            date_collected=datetime.date(2017, 1, 1),
+            achieved=42,
+            record_name='My Test Record',
+            evidence_url='http://my_evidence_url',
+        )
+
+    def test_result_table_html(self):
+        url = reverse_lazy('result_view', args=[self.indicator.id, self.program.id])
+        response = self.client.get(url)
+        # result is displayed
+        self.assertContains(
+            response,
+            'Jan 1, 2017',
+        )
+        self.assertContains(response, '42', count=3)
+        # expected warning message
+        self.assertContains(
+            response,
+            'Targets are not set up for this indicator.'
+        )
+        self.assertContains(
+            response,
+            'Start by selecting a target frequency.'
         )
