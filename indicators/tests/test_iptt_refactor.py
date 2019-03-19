4ebc6b24b88ac15ee6ff919f7427daaf95ba7409
Cameron Mcfee <cameron.mcfee@gmail.com>
2019-03-19 03:08:10 -0700

    tests written for new iptt model

--- indicators/tests/test_iptt_refactor.py
+++ indicators/tests/test_iptt_refactor.py
@@ -7,7 +7,11 @@ Unit:
             - frequencies (in correct language)
 
 """
+from indicators.tests.iptt_tests.tp_indicator_counts import (
+    TestIPTTTimeperiodsValues
+)
 
+"""
 from indicators.tests.iptt_tests.quickstart_unit import (
     TestIPTTQuickstartJSContext
 )
@@ -16,6 +20,35 @@ from indicators.tests.iptt_tests.reportview_unit import (
     TestIPTTReportviewURL
 )
 
-from indicators.tests.iptt_tests.targetperiod_lop_values import (
-    TestIPTTReportResponseCounts
+from indicators.tests.iptt_tests.tva_indicator_counts import (
+    TestIPTTReportResponseCountsLOP,
+    TestIPTTReportResponseCountsMidEnd,
+    TestIPTTReportResponseCountsAnnual,
+    TestIPTTReportResponseCountsMonthly
 )
+
+from indicators.tests.iptt_tests.tva_indicator_lop_values import (
+    TestSingleLOPNumNCIndicatorIPTTResponse,
+    TestSingleMidendNumNCIndicatorIPTTResponse,
+    TestSingleSemiAnnualNumNCIndicatorIPTTResponse,
+    TestSingleQuarterlyNumNCIndicatorIPTTResponse,
+    TestSingleLOPNumCumIndicatorIPTTResponse,
+    TestSingleLOPPercentIndicatorIPTTResponse,
+    TestSingleMidEndPercentIndicatorIPTTResponse,
+    TestSingleTriannualPercentIndicatorIPTTResponse,
+    TestMultipleLOPIndicatorsIPTTResponse,
+    TestMultipleQuarterlyIndicatorsIPTTResponse,
+    TestMultipleCumulativeIndicatorsIPTTResponse,
+    TestMultiplePercentLOPIndicatorsIPTTResponse,
+    TestMultiplePercentAnnualIndicatorsIPTTResponse
+)
+
+from indicators.tests.iptt_tests.tva_report_tests import (
+    TestMidEndTVAReportValues,
+    TestSemiannualTVAReportValues
+)
+
+from indicators.tests.iptt_tests.iptt_reports_i18n import (
+    TestIPTTTimePeriodReportsI18N,
+    TestIPTTTargetReportsI18N
+)"""
