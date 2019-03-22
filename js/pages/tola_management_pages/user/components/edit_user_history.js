ae3b78c81d92ddce05c2e209afdf219ac7acfd6c
Paul Souders <paul@axoplasm.com>
2019-03-22 10:13:52 -0700

    partial #1272: History pane on edit user screen

--- js/pages/tola_management_pages/user/components/edit_user_history.js
+++ js/pages/tola_management_pages/user/components/edit_user_history.js
@@ -72,85 +72,75 @@ export class EditUserHistory extends React.Component {
     render() {
         const selected = status_options.find(option => option.value == this.state.user_data.user.is_active)
         const {history} = this.props
-        return <div className="edit-user-history">
+        return <div className="admin-edit-pane">
             <h2 className="no-bold">{this.state.user_data.name?this.state.user_data.name+': ':''}{gettext("Status and History")}</h2>
-            <div className="row">
-                <div className="col">
-                    <Select isDisabled={this.props.disabled} options={status_options} value={selected} onChange={(new_value) => this.onChange(new_value)} />
-                </div>
+            <div className="form-group">
+                <Select isDisabled={this.props.disabled} options={status_options} value={selected} onChange={(new_value) => this.onChange(new_value)} />
             </div>
-            <div className="row">
-                <div className="col">
-                    <button className="btn btn-secondary" onClick={() => this.onResendRegistrationEmail()}>{gettext("Resend Registration Email")}</button>
-                </div>
+            <div className="form-group">
+                <button className="btn btn-secondary" onClick={() => this.onResendRegistrationEmail()}>{gettext("Resend Registration Email")}</button>
             </div>
-            <div className="row">
-                {!this.props.disabled &&
-                <div className="col">
+            {!this.props.disabled &&
+                <div className="form-group">
                     <div className="form-group btn-row">
                         <button className="btn btn-primary" type="button" onClick={() => this.props.onSave(this.state.user_data)}>{gettext("Save Changes")}</button>
                         <button className="btn btn-reset" type="button" onClick={() => this.onReset()}>{gettext("Reset")}</button>
                     </div>
                 </div>
-                }
-            </div>
-            <div className="row">
-                <div className="col">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>{gettext("Date")}</th>
-                                <th>{gettext("Admin")}</th>
-                                <th>{gettext("Change Type")}</th>
-                                <th>{gettext("Previous Entry")}</th>
-                                <th>{gettext("New Entry")}</th>
+            }
+            <table className="table table-sm">
+                <thead>
+                    <tr>
+                        <th>{gettext("Date")}</th>
+                        <th>{gettext("Admin")}</th>
+                        <th>{gettext("Change Type")}</th>
+                        <th>{gettext("Previous Entry")}</th>
+                        <th>{gettext("New Entry")}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {history.map(entry => {
+                        if(entry.change_type == 'user_programs_updated') {
+                            return <tr key={entry.id}>
+                                <td className="text-nowrap">{entry.date}</td>
+                                <td>{entry.admin_user}</td>
+                                <td>{entry.change_type}</td>
+                                <td className="expand-section">
+                                    <Expander>
+                                        <ProgramChangesetEntry data={entry.diff_list} timeframe="prev"/>
+                                    </Expander>
+                                </td>
+                                <td className="expand-section">
+                                    <Expander>
+                                        <ProgramChangesetEntry data={entry.diff_list} timeframe="new" />
+                                    </Expander>
+                                </td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            {history.map(entry => {
-                                if(entry.change_type == 'user_programs_updated') {
-                                    return <tr key={entry.id}>
-                                        <td>{entry.date}</td>
-                                        <td>{entry.admin_user}</td>
-                                        <td>{entry.change_type}</td>
-                                        <td className="expand-section">
-                                            <Expander>
-                                                <ProgramChangesetEntry data={entry.diff_list} timeframe="prev"/>
-                                            </Expander>
-                                        </td>
-                                        <td className="expand-section">
-                                            <Expander>
-                                                <ProgramChangesetEntry data={entry.diff_list} timeframe="new" />
-                                            </Expander>
-                                        </td>
-                                    </tr>
-                                } else {
+                        } else {
 
-                                    return <tr key={entry.id}>
-                                        <td>{entry.date}</td>
-                                        <td>{entry.admin_user}</td>
-                                        <td>{entry.change_type}</td>
-                                        <td className="expand-section">
-                                            <Expander>
-                                                {entry.diff_list.map(changeset => {
-                                                        return <ChangesetEntry key={changeset.name} name={changeset.name} type={entry.change_type} data={changeset.prev} />
-                                                })}
-                                            </Expander>
-                                        </td>
-                                        <td className="expand-section">
-                                            <Expander>
-                                                {entry.diff_list.map(changeset => {
-                                                        return <ChangesetEntry key={changeset.name} name={changeset.name} type={entry.change_type} data={changeset.new} />
-                                                })}
-                                            </Expander>
-                                        </td>
-                                    </tr>
-                                }
-                            })}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+                            return <tr key={entry.id}>
+                                <td className="text-nowrap">{entry.date}</td>
+                                <td>{entry.admin_user}</td>
+                                <td>{entry.change_type}</td>
+                                <td className="expand-section">
+                                    <Expander>
+                                        {entry.diff_list.map(changeset => {
+                                                return <ChangesetEntry key={changeset.name} name={changeset.name} type={entry.change_type} data={changeset.prev} />
+                                        })}
+                                    </Expander>
+                                </td>
+                                <td className="expand-section">
+                                    <Expander>
+                                        {entry.diff_list.map(changeset => {
+                                                return <ChangesetEntry key={changeset.name} name={changeset.name} type={entry.change_type} data={changeset.new} />
+                                        })}
+                                    </Expander>
+                                </td>
+                            </tr>
+                        }
+                    })}
+                </tbody>
+            </table>
         </div>
     }
 }
