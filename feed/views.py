56f218bfe26d3b1dfbd2ea0262ff3371e76c0819
Ken Johnson <kjohnson@mercycorps.org>
2019-04-08 15:43:13 -0700

    ProgramTargetFrequencies permission check

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,4 @@
+from tola_management.permissions import verify_program_access_level
 from .serializers import (
     PeriodicTargetSerializer, ProgramIndicatorSerializer, ProgramSerializer, UserSerializer,
     SectorSerializer, ProjectTypeSerializer, OfficeSerializer, SiteProfileSerializer, CountrySerializer,
@@ -581,6 +582,7 @@ class OrganizationViewSet(APIDefaultsMixin, viewsets.ModelViewSet):
 class ProgramTargetFrequencies(viewsets.ViewSet):
     def list(self, request):
         program = get_object_or_404(Program, pk=request.query_params.get('program_id', None))
+        verify_program_access_level(request, program.id, 'low')
         queryset = program.indicator_set.exclude(
             models.Q(target_frequency=Indicator.EVENT) | models.Q(target_frequency__isnull=True)
             ).values('target_frequency').distinct().order_by('target_frequency')
