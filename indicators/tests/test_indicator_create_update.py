a77ce49e7cb4cbe7fe757b9729a749f618bcca5a
Ken Johnson <risinglight@gmail.com>
2019-05-24 15:14:12 -0700

    Unit tests for creation/update of indicator form endpoints

--- indicators/tests/test_indicator_create_update.py
+++ indicators/tests/test_indicator_create_update.py
@@ -1,64 +1,178 @@
+import json
+
 from django.test import RequestFactory, TestCase
 from django.urls import reverse_lazy
 
-from indicators.models import Indicator
+from factories import ResultFactory
+from indicators.models import Indicator, PeriodicTarget
 from tola.test.base_classes import TestBase
 
 
-class IndicatorCreateFunctionTests(TestBase, TestCase):
-
-    def setUp(self):
-        super(IndicatorCreateFunctionTests, self).setUp()
-
-    def test_get(self):
-        url = reverse_lazy('indicator_create', args=[self.program.id])
-        response = self.client.get(url)
-
-        # self.assertContains(response, 'Indicator Performance Tracking Table')
-        self.assertTemplateUsed(response, 'indicators/indicator_form_modal.html')
-
-    # def test_post(self):
-    #     request_content = {
-    #         'program': self.program.id, 'country': self.country.id, 'services': 0, 'service_indicator': 0}
-    #     response = self.client.post('/indicators/indicator_create/%s/' % self.program.id, request_content)
-    #
-    #     self.assertEqual(response.status_code, 302)
-
-
-class IndicatorUpdateTests(TestBase, TestCase):
-
-    def setUp(self):
-        super(IndicatorUpdateTests, self).setUp()
-
-    def test_get(self):
-        url = reverse_lazy('indicator_update', args=[self.indicator.id])
-        response = self.client.get(url)
-
-        # self.assertContains(response, 'Indicator Performance Tracking Table')
-        self.assertTemplateUsed(response, 'indicators/indicator_form_modal.html')
-
-    def test_post(self):
-
-        # build form data using URL encoded form key value pairs
-        data = {
-            'name': 'Test+Name',
+class TestIndcatorCreateUpdateBase(TestBase):
+    def _base_indicator_post_data(self, target_frequency, periodic_targets):
+        return {
+            'name': 'Test Indicator',
             'program_id': self.program.id,
-            'target_frequency': Indicator.ANNUAL,
+            'target_frequency': target_frequency,
             'level': self.level.id,
             'indicator_type': 1,
             'unit_of_measure_type': 1,
             'unit_of_measure': 1,
             'lop_target': 3223,
             'direction_of_change': Indicator.DIRECTION_OF_CHANGE_NONE,
-            'periodic_targets': '[]',
+            'periodic_targets': json.dumps(periodic_targets),
+            'rationale': 'foo',
         }
-        request = RequestFactory()
-        request.user = self.user
+
+
+
+class IndicatorCreateTests(TestIndcatorCreateUpdateBase, TestCase):
+    """
+    Test the create indicator form api paths works, and PTs are created
+    """
+
+    def setUp(self):
+        super(IndicatorCreateTests, self).setUp()
+
+        self.indicator.delete()  # scrap this since we are making new indicators
+
+    def test_get(self):
+        url = reverse_lazy('indicator_create', args=[self.program.id])
+        response = self.client.get(url)
+
+        self.assertTemplateUsed(response, 'indicators/indicator_form_modal.html')
+
+    def test_lop_creation(self):
+        periodic_targets = []
+        data = self._base_indicator_post_data(Indicator.LOP, periodic_targets)
+
+        self.assertEqual(Indicator.objects.count(), 0)
+        self.assertEqual(PeriodicTarget.objects.count(), 0)
+
+        url = reverse_lazy('indicator_create', args=[self.program.id])
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(Indicator.objects.count(), 1)
+        self.assertEqual(PeriodicTarget.objects.count(), 1)
+
+        indicator = Indicator.objects.get()
+        pt = PeriodicTarget.objects.get()
+
+        self.assertEqual(pt.indicator, indicator)
+        self.assertEqual(pt.period_name, PeriodicTarget.LOP_PERIOD)
+        self.assertEqual(pt.target, indicator.lop_target)
+
+    def test_annual_creation(self):
+        periodic_targets = [
+            {"id": 0, "period": "Year 1", "target": "1", "start_date": "Jan 1, 2018", "end_date": "Dec 31, 2018"},
+            {"id": 0, "period": "Year 2", "target": "2", "start_date": "Jan 1, 2019", "end_date": "Dec 31, 2019"},
+            {"id": 0, "period": "Year 3", "target": "3", "start_date": "Jan 1, 2020", "end_date": "Dec 31, 2020"}]
+
+        data = self._base_indicator_post_data(Indicator.ANNUAL, periodic_targets)
+
+        self.assertEqual(Indicator.objects.count(), 0)
+        self.assertEqual(PeriodicTarget.objects.count(), 0)
+
+        url = reverse_lazy('indicator_create', args=[self.program.id])
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(Indicator.objects.count(), 1)
+        self.assertEqual(PeriodicTarget.objects.count(), 3)
+
+        indicator = Indicator.objects.get()
+        pt = PeriodicTarget.objects.order_by('start_date').first()
+
+        self.assertEqual(pt.indicator, indicator)
+        self.assertEqual(pt.period_name, 'Year 1')
+        self.assertEqual(pt.target, 1)
+
+    def test_events_creation(self):
+        periodic_targets = [{"id": 0, "period": "a", "target": "1", "start_date": "", "end_date": ""},
+                            {"id": 0, "period": "b", "target": "2"}]
+
+        data = self._base_indicator_post_data(Indicator.EVENT, periodic_targets)
+
+        self.assertEqual(Indicator.objects.count(), 0)
+        self.assertEqual(PeriodicTarget.objects.count(), 0)
+
+        url = reverse_lazy('indicator_create', args=[self.program.id])
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(Indicator.objects.count(), 1)
+        self.assertEqual(PeriodicTarget.objects.count(), 2)
+
+        indicator = Indicator.objects.get()
+        pt = PeriodicTarget.objects.order_by('customsort').first()
+
+        self.assertEqual(pt.indicator, indicator)
+        self.assertEqual(pt.period_name, 'a')
+        self.assertEqual(pt.target, 1)
+
+
+
+class IndicatorUpdateTests(TestIndcatorCreateUpdateBase, TestCase):
+    """
+    Test the update form API works, PTs are created, and that results are reassigned
+    """
+
+    def setUp(self):
+        super(IndicatorUpdateTests, self).setUp()
+
+        self.result = ResultFactory(
+            periodic_target=None,
+            indicator=self.indicator,
+            program=self.program,
+            achieved=1024,
+            date_collected='2018-06-01'
+        )
+
+    def test_get(self):
+        url = reverse_lazy('indicator_update', args=[self.indicator.id])
+        response = self.client.get(url)
+
+        # self.assertContains(response, 'Indicator Performance Tracking Table')
+        self.assertTemplateUsed(response, 'indicators/indicator_form_modal.html')
+
+    def test_lop_update(self):
+        data = self._base_indicator_post_data(Indicator.LOP, [])
+
+        self.assertEqual(PeriodicTarget.objects.count(), 0)
 
         url = reverse_lazy('indicator_update', args=[self.indicator.id])
         response = self.client.post(url, data)
 
         self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(PeriodicTarget.objects.count(), 1)
+
+        self.result.refresh_from_db()
+        self.assertEqual(self.result.periodic_target, PeriodicTarget.objects.get())
+
+    def test_annual_update(self):
+        periodic_targets = [
+            {"id": 0, "period": "Year 1", "target": "1", "start_date": "Jan 1, 2018", "end_date": "Dec 31, 2018"},
+            {"id": 0, "period": "Year 2", "target": "2", "start_date": "Jan 1, 2019", "end_date": "Dec 31, 2019"},
+            {"id": 0, "period": "Year 3", "target": "3", "start_date": "Jan 1, 2020", "end_date": "Dec 31, 2020"}]
+
+        data = self._base_indicator_post_data(Indicator.ANNUAL, periodic_targets)
+
+        self.assertEqual(PeriodicTarget.objects.count(), 0)
+
+        url = reverse_lazy('indicator_update', args=[self.indicator.id])
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(PeriodicTarget.objects.count(), 3)
+
+        self.result.refresh_from_db()
+        self.assertEqual(self.result.periodic_target, PeriodicTarget.objects.order_by('start_date').first())
 
 
 class PeriodicTargetsFormTests(TestBase, TestCase):
