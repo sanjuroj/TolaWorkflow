eccf9ad7a090c9a57d340e9532e7c34e901ab70f
Cameron Mcfee <cameron.mcfee@gmail.com>
2019-06-10 12:09:40 -0700

    cleanup and handling the readonly attribute

--- js/components/indicatorModalComponents.js
+++ js/components/indicatorModalComponents.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 
-/*
- *{!readonly &&
-            <a role="button" className="btn-link btn-add" onClick={e => openCreateIndicatorFormModal(programId)}>
+export const AddIndicatorButton = observer(({ readonly, ...params }) => {
+    if (readonly) {
+        // do we want to show a message of some kind to folks for whom this button is disabled or just hide it?
+        return '';
+    }
+    return (
+            <a role="button" className="btn-link btn-add" onClick={e => openCreateIndicatorFormModal(params)}>
                 <i className="fas fa-plus-circle"/> {gettext("Add indicator")}
             </a>
-            */
-
-export const AddIndicatorButton = observer(({ readonly, ...params }) => {
-    return (
-        <a role="button" className="btn-link btn-add" onClick={e => openCreateIndicatorFormModal(params)}>
-            <i className="fas fa-plus-circle"/> {gettext("Add indicator")}
-        </a>
     );
 });
