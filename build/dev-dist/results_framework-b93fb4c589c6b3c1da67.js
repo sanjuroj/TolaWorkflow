031ae458731108d911ddc120ead1a4725b6e9a6f
Ken Johnson <kjohnson@mercycorps.org>
2019-06-27 11:32:55 -0700

    Build JS

--- build/dev-dist/results_framework-b93fb4c589c6b3c1da67.js
+++ build/dev-dist/results_framework-b93fb4c589c6b3c1da67.js
@@ -787,7 +787,7 @@ function (_React$Component4) {
         target.addClass("is-invalid");
         /* # Translators: This is a validation message given to the user when the user-editable name field has been deleted or omitted. */
 
-        var feedbackText = "Please provide a name for this ".concat(_this4.props.levelProps.tierName);
+        var feedbackText = gettext('Please complete this field');
         target.after("<p id=name-feedback-".concat(_this4.props.level.id, " class=\"invalid-feedback\">").concat(feedbackText, "</p>"));
       } else {
         $("#level-name-".concat(_this4.props.level.id)).removeClass("is-invalid");
@@ -2707,4 +2707,4 @@ var STATUS_CODES = {
 /***/ })
 
 },[["QTZG","runtime","vendors"]]]);
-//# sourceMappingURL=results_framework-aadc8020ac6b74f9ae51.js.map
+//# sourceMappingURL=results_framework-b93fb4c589c6b3c1da67.js.map
